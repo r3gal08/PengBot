2fa57baf6ad7f08ff2732dc875613969070bdca2
r3gal08 <brettleard808@gmail.com>
2024-10-01 17:48:54 -0300

    Initial branch commit

--- examples/ollama_test.py
+++
@@ -1,6 +0,0 @@
-from langchain_ollama import OllamaLLM
-
-llm = OllamaLLM(model="llama3.1")
-
-response = llm.invoke("tell me a little story while I monitor top and nvtop and view my GPU usage when using this model")
-print(response)
